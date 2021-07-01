7d34e3ea67b1697061aa5cd8e5013456c63dde74
terada0628 <ntpeanuts@yahoo.co.jp>
2021-07-01 11:11:08 +0000

    finish2

--- bookers2-debug/app/controllers/users_controller.rb
+++ bookers2-debug/app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :ensure_correct_user, {only: [:edit,:update]}
+    before_action :authenticate_user!
 
   def show
     @user = User.find(params[:id])
